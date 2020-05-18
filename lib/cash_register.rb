bd0c67fbc1b308ee0c47c7034d4ea893b01ef042
Richard Hanolt <rhanolt@gmail.com>
2020-05-18 22:43:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,10 +18,10 @@ class CashRegister
   end
 
   def apply_discount
-    if @discount = 20
+    if @discount == 20
       @total = @total*(1 - @discount/100.0)
       return "After the discount, the total comes to $#{@total.to_i}."
-    elsif @discount = 0
+    elsif @discount == 0
       return "There is no discount to apply."
     end
   end
